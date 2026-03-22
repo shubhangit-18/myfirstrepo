edd5bbdfdac5eafb9d21b7896fac597f933a8a6a
shubhangit-18 <siddhissingh09@gmail.com>
2026-03-22 10:32:27 +0530

    Update solution2.py

--- solution2.py
+++ solution2.py
@@ -1,4 +1,4 @@
 weight=float(input("Enter your weight in kg:"))
-height=float(input("Enter your height in meters:"))
+height=float(input("Entertg54 your height in meters:"))
 bmi=weight/(height**2)
 print("Your BMI is:",bmi)
